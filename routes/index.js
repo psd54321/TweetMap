dadd446be5dec97422fe51844e2992846c827a2c
prathameshdinkar <psd54321@gmail.com>
2016-10-27 16:53:27 -0400

    change

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var Twitter = require('twit');
 var Elasticsearch = require('aws-es');
 
 var client = new Twitter({
-    consumer_key: 'Your ley',
-    consumer_secret: 'Your key',
-    access_token: 'Your key',
-    access_token_secret: 'Your key'
+    consumer_key: '1hkOv5wjdTaqxsRM91FGwjkRU',
+    consumer_secret: '04VmYHw6z56sSt1Ypm6BbgxweTLiM5ejFxdusWU7AqYYtP8Lf6',
+    access_token: '218215924-qk6PxJv342X46C3AwMNFFCI5raOa7coBYCFRXmGo',
+    access_token_secret: 'pKQKTqsm37kSFVysuKodhZ7BusEKZ8J9dS7Pv3jKjB4bY'
 });
 
 /* GET home page */
@@ -19,8 +19,8 @@ router.get('/', function (req, res) {
 
 router.get('/search/:searchq', function (req, res) {
     var elasticsearch = new Elasticsearch({
-        accessKeyId: 'Your key',
-        secretAccessKey: 'Your key',
+        accessKeyId: 'AKIAIFPUJ5RGB2WHFGFQ',
+        secretAccessKey: 's1DQkT7gLzeEC33OukEn2iv2jAFaacVyiOJcciW0',
         service: 'es',
         region: 'us-east-1',
         host: 'search-prathtweets-jqcnx3spdjo3rhy5zjzn4nusv4.us-east-1.es.amazonaws.com'
